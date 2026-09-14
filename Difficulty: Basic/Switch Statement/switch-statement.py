dbcbd2e33418ca0795da3ab70bbbfd9c64fbf652
Vishu Kumar <vishu.singh.cse.2025@miet.ac.in>
2026-09-14 17:54:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Switch Statement/switch-statement.py
@@ -0,0 +1,25 @@
+
+# code here
+n = int(input())
+
+match n:
+    case 1:
+        print("One")
+    case 2:
+        print("Two")
+    case 3:
+        print("Three")
+    case 4:
+        print("Four")
+    case 5:
+        print("Five")
+    case 6:
+        print("Six")
+    case 7:
+        print("Seven")
+    case 8:
+        print("Eight")
+    case 9:
+        print("Nine")
+    case _:
+        print("Unknown")
